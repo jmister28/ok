a6d8ce0a7c9b0a7e0b6e76e8cc33bc775ce8ee53
jmister28 <jmister28@users.noreply.github.com>
2020-01-24 14:08:26 -0500

    Update blockchain.ts

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,1 +1,11 @@
-
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public timestamp: number;
+  public data string;
+  
+  constructor(index: number, hash: string, previousHash: string, timestamp: number){
+    
+  }
+}
