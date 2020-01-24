7dec144de2ad733c2ce4561fefdf137bd500ef6f
jmister28 <jmister28@users.noreply.github.com>
2020-01-24 15:01:08 -0500

    Update blockchain.ts

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -20,6 +20,9 @@ const genesisBlock: Block = new Block(
   0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', '', 1465154705, 'Genesis Block Yay' 
 ); //define genesis block
 
+//define blockchain as array
+const blockchain: Block[] = [genesisBlock];
+
 //generate new block function
 const generateNextBlock = (blockData: string) => {
   const previousBlock: Block = getLatestBlock(); // most recent block before the one we are about to create
@@ -29,10 +32,18 @@ const generateNextBlock = (blockData: string) => {
   const newBlock: Block = new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
   return newBlock;
 }
-
 //end function
 
 // Calculate current block hash using CryptoJS
 const calculateHash(index: number, previousHash: string, timestamp: number, data: string): string => 
   CyptoJS.SHA256(index + previousHash + timestamp + data).toString();
 //end function
+
+//validate blocks
+const isValidNewBlock = (newBlock: Block, previousBlock: Block) => {
+  if (previousBlock.index + 1 !== newBlock.index){
+    console.log("");
+    return false;
+  }
+  return true;
+}
