b534cb4acfc259e983c6a25065e6daef762ab7a3
jmister28 <jmister28@users.noreply.github.com>
2020-01-25 15:07:51 -0500

    Update blockchain.ts

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -23,7 +23,7 @@ class Block {
 }
 
 const genesisBlock: Block = new Block(
-  0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', '', 1465154705, 'Genesis Block Yay' 
+  0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', '', 1465154705, 'Genesis Block Yay',0,0 
 ); //define genesis block
 
 //define blockchain as array
