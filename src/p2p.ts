2ae1ca23af6eb79b46bfc818563bd247aeea342a
jmister28 <jmister28@users.noreply.github.com>
2020-01-24 21:18:04 -0500

    Update p2p.ts

--- src/p2p.ts
+++ src/p2p.ts
@@ -1,1 +1,17 @@
+import * as WebSocket from 'ws';
+import {Server} from 'ws';
+import {addBlockToChain, Block, getBlockchain, getLatestBlock, isValidBlockStructure, replaceChain} from './blockchain';
+
+const sockets: WebSocket[] = [];
+
+enum MessageType {
+  QUERY_LATEST = 0,
+  QUERY_ALL = 1,
+  RESPONSE_BLOCKCHAIN = 2,
+}
+
+class Message {
+  public type : MessageType;
+  public data : any;
+}
 
